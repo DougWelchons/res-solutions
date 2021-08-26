b623b069b88ed8b8ff2b76d159cf9fd9101193fc
DougWelchons <doug.welchons@gmail.com>
2021-08-26 17:38:31 -0600

    Add login page

---
+++ spec/features/login/index_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "Login Index page" do
+  describe "happy path" do
+    it "has a button to log in via microsoft_office365" do
+      visit root_path
+
+      expect(page).to have_button("login")
+    end
+  end
+end
