971bc051bcae4b89c437b904da238134c73dd830
DougWelchons <doug.welchons@gmail.com>
2021-08-31 11:20:29 -0600

    Add modle testing for sections, sub-sections, and parts

---
+++ spec/models/part_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Part, type: :model do
+  describe "relationships" do
+    it { should belong_to(:user) }
+    it { should belong_to(:sub_section) }
+    it { should have_one(:section).through(:sub_section) }
+    it { should have_one(:report).through(:section) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:section_number) }
+    # it { should validate_uniqueness_of(:section_number).scoped_to(:sub_section_id) }
+  end
+end
