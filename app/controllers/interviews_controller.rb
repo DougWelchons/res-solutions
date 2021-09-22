177d0615f958d508a616a04831c0b8ba5e20b74b
DougWelchons <doug.welchons@gmail.com>
2021-09-22 11:14:50 -0600

    Add nav-bar to each page

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,21 +1,43 @@
 class InterviewsController < ApplicationController
   before_action :current_user
 
-
   def index
     if params[:report_id]
-      @interviews = Report.find(params[:report_id]).interviews
+      report = Report.find(params[:report_id])
+      @interviews = report.interviews
+      @nav_bar = {
+        logout: {title: "Logout", path: logout_path, method: :delete},
+        reports: {title: "Home", path: reports_path, method: :get},
+        report: {title: "Report", path: report_path(report), method: :get}
+      }
     elsif params[:section_id]
-      @interviews = Section.find(params[:section_id]).interviews
+      section = Section.find(params[:section_id])
+      @interviews = section.interviews
+      @nav_bar = {
+        logout: {title: "Logout", path: logout_path, method: :delete},
+        reports: {title: "Home", path: reports_path, method: :get},
+        report: {title: "Report", path: report_path(section.report), method: :get},
+        section: {title: "Section", path: section_path(section), method: :get},
+      }
     else
       @interviews = Interview.all
+      @nav_bar = {
+        logout: {title: "Logout", path: logout_path, method: :delete},
+        reports: {title: "Home", path: reports_path, method: :get}
+      }
     end
   end
 
   def show
     @interview = Interview.find(params[:id])
+    @nav_bar = {
+      logout: {title: "Logout", path: logout_path, method: :delete},
+      reports: {title: "Home", path: reports_path, method: :get},
+      report: {title: "Report", path: report_path(@interview.report), method: :get},
+      section: {title: "Section", path: section_path(@interview.section), method: :get},
+    }
   end
-  
+
   def new
   end
 
--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -3,5 +3,10 @@ class SectionsController < ApplicationController
 
   def show
     @section = Section.find(params[:id])
+    @nav_bar = {
+      logout: {title: "Logout", path: logout_path, method: :delete},
+      reports: {title: "Home", path: reports_path, method: :get},
+      report: {title: "Report", path: report_path(@section.report), method: :get}
+    }
   end
 end
